dd945e1183097fdb30f0a5fc6b7455173fb4df43
Hyeongho <rlaguddgh@nate.com>
2023-07-03 02:42:36 +0900

    230702
    프로그래머스
     - 이상한 문자 만들기(lv1)

---
+++ 230702/이상한 문자 만들기.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+
+    int idx = 0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            answer += s[i];
+
+            idx = 0;
+
+            continue;
+        }
+
+        if (idx % 2 == 0)
+        {
+            answer += toupper(s[i]);
+
+            idx++;
+        }
+
+        else
+        {
+            answer += tolower(s[i]);
+
+            idx++;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution("try hello world");
+
+    return 0;
+}
