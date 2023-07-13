53a6a0cb1b1d780d0cfdaec9b17e34f0204b5b8c
Hyeongho <rlaguddgh@nate.com>
2023-07-13 18:23:50 +0900

    230713
    프로그래머스
     - 하노이의 탑(lv2)

---
+++ 230713/하노이의 탑.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void Hanoi(vector<vector<int>>& answer, int n, int from, int by, int to)
+{
+    if (n == 1)
+    {
+        answer.push_back({ from, to });
+
+        return;
+    }
+
+    Hanoi(answer, n - 1, from, to, by);
+
+    answer.push_back({ from, to });
+
+    Hanoi(answer, n - 1, by, from, to);
+}
+
+vector<vector<int>> solution(int n) {
+    vector<vector<int>> answer;
+
+    Hanoi(answer, n, 1, 2, 3);
+
+    return answer;
+}
